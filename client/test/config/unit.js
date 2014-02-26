7932689a8a95dae53cecafd0a3a90258265b8b36
David Wilson <david.wilson@nreca.coop>
2014-02-26 16:42:52 -0500

    update to latest karma config

--- client/test/config/unit.js
+++ client/test/config/unit.js
@@ -1,47 +1,49 @@
+module.exports = function (config) {
+    config.set({
 // base path, that will be used to resolve files and exclude
-basePath = '../..';
+        basePath: '',
 
 // list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'vendor/jquery/jquery.js',
-  'vendor/angular/angular.js',
-  'vendor/angular/angular-route.js',
-  'vendor/mongolab/mongolab-resource.js',
-  'test/vendor/angular/angular-mocks.js',
-  'vendor/angular-ui/**/*.js',
-  'src/**/*.js',
-  'test/unit/**/*.spec.js',
-  'dist/templates/**/*.js'
-];
+        files: [
+            '../../vendor/jquery/jquery.js',
+            '../../vendor/angular/angular.js',
+            '../../vendor/angular/angular-route.js',
+            '../../vendor/mongolab/mongolab-resource.js',
+            '../../test/vendor/angular/angular-mocks.js',
+            '../../vendor/angular-ui/**/*.js',
+            '../../src/**/*.js',
+            '../../test/unit/**/*.spec.js',
+            '../../dist/templates/**/*.js'
+        ],
+
+        frameworks: ["jasmine"],
 
 // use dots reporter, as travis terminal does not support escaping sequences
 // possible values: 'dots' || 'progress'
-reporters = 'progress';
+        //reporters: 'dots',
 
 // these are default values, just to show available options
 
 // web server port
-port = 8089;
+        port: 8089,
 
 // cli runner port
-runnerPort = 9109;
+        runnerPort: 9109,
 
-urlRoot = '/__test/';
+        urlRoot: '/__test/',
 
 // enable / disable colors in the output (reporters and logs)
-colors = true;
+        colors: true,
 
 // level of logging
 // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+        logLevel: config.LOG_INFO,
 
 // enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+        autoWatch: false,
 
 // polling interval in ms (ignored on OS that support inotify)
-autoWatchInterval = 0;
+        autoWatchInterval: 0,
 
 // Start these browsers, currently available:
 // - Chrome
@@ -50,8 +52,10 @@ autoWatchInterval = 0;
 // - Opera
 // - Safari
 // - PhantomJS
-browsers = ['Chrome'];
+        browsers: ['Chrome'],
 
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
-singleRun = true;
+        singleRun: true
+    });
+};
